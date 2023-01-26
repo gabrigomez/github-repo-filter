5500f8c7df5a34870d3b14dbe6c584173b058104
Gabriel <gabrigomez15@gmail.com>
2023-01-26 16:13:44 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+interface Repo {
+  name: string;
+  description: string;
+}
+
+interface AppProps {
+  list: number[];
+}
 
 function App() {
+  const [repos, setRepos] = useState<Repo[]>([])
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/gabrigomez/repos')
+      .then(response => response.json())
+      .then(data => setRepos(data))
+  }, [])
+
+  const filteredRepos = search.length > 0
+    ? repos.filter(repo => repo.name.includes(search))
+    : []
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        name='search' 
+        type="text"
+        placeholder='buscar'
+        onChange={e => setSearch(e.target.value)}
+        value={search}
+        />
+      
+      {search.length > 0 ? ( 
+        <ul>
+          {filteredRepos.map(repo => {
+            return (
+              <li key={repo.name}>
+                {repo.name}
+              </li>
+            )
+          })}
+        </ul>
+      ): (
+        <ul>
+          {repos.map(repo => {
+            return (
+              <li key={repo.name}>
+                {repo.name}
+              </li>
+            )
+          })}
+        </ul>
+       )}
     </div>
   );
 }
