a15885ab9a34c339e47f37a95202caf9289e39e8
Gabriel <gabrigomez15@gmail.com>
2023-01-27 11:49:56 -0300

    feat: add basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -30,65 +30,69 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App flex flex-col items-center mt-20">
       <div>
-        <form onSubmit={handleSubmit}>
-          <input 
-            name='user'
-            type="text" 
-            placeholder='Digite o nome do usuário'
-            value={user}
-            onChange={e => setUser(e.target.value)}
-          />
-          <button>
-            Buscar
-          </button>
-        </form>
-      </div>
-      {repos.length > 0 ? (
-        <div>
-          <input
-            name='search' 
-            type="text"
-            placeholder='Busque o repositório'
-            onChange={e => setSearch(e.target.value)}
-            value={search}
-            />          
-          {search.length > 0 ? ( 
-            <ul>
-              {filteredRepos.map(repo => {
-                return (
-                  <li key={repo.name}>
-                    {repo.name}                                        
-                  </li>
-                )
-              })}
-            </ul>
-          ) : (
-            <ul>
-              {repos.map(repo => {
-                return (
-                  <li key={repo.name}>
-                    {repo.name}
-                  </li>
-                )
-              })}
-            </ul>
-          )}
+        <div className='mb-4'>
+          <form onSubmit={handleSubmit}>
+            <input 
+              name='search-user'
+              className='border-b border-black'
+              type="text" 
+              placeholder='Digite o nome do usuário'
+              value={user}
+              onChange={e => setUser(e.target.value)}
+            />
+            <button className='ml-4 p-1 bg-slate-300 rounded-md'>
+              Buscar
+            </button>
+          </form>
         </div>
-      
-      ) : (
-        <div>
-          {error ? (
-            <div>
-              Usuário não encontrado
-            </div>
-          ) : (
-            <>
-            </>
-          )}        
-        </div>
-      )}      
+        {repos.length > 0 ? (
+          <div>
+            <input
+              name='search-repo'
+              className='border-b border-black mb-4' 
+              type="text"
+              placeholder='Filtre o repositório'
+              onChange={e => setSearch(e.target.value)}
+              value={search}
+              />          
+            {search.length > 0 ? ( 
+              <ul>
+                {filteredRepos.map(repo => {
+                  return (
+                    <li key={repo.name}>
+                      {repo.name}                                        
+                    </li>
+                  )
+                })}
+              </ul>
+            ) : (
+              <ul>
+                {repos.map(repo => {
+                  return (
+                    <li key={repo.name}>
+                      {repo.name}
+                    </li>
+                  )
+                })}
+              </ul>
+            )}
+          </div>
+        
+        ) : (
+          <div>
+            {error ? (
+              <div>
+                Usuário não encontrado
+              </div>
+            ) : (
+              <>
+              </>
+            )}        
+          </div>
+        )}      
+      </div>      
     </div>
   );
 }
