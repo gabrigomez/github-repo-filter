a567824382edc11bba117d04f23d30308294bf1f
Gabriel <gabrigomez15@gmail.com>
2023-01-30 11:50:55 -0300

    feat: add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { GithubLogo, MagnifyingGlass } from 'phosphor-react';
+import { CircleNotch, GithubLogo, MagnifyingGlass } from 'phosphor-react';
 
 interface Repo {
   name: string;
@@ -16,10 +16,12 @@ interface User {
 
 function App() {
   const [repos, setRepos] = useState<Repo[]>([])
+  const [user, setUser] = useState<User>({public_repos: 0, avatar_url: '', login: '', html_url: ''})
   const [search, setSearch] = useState('');
   const [userName, setUserName] = useState('');
+
   const [error, setError] = useState('');
-  const [user, setUser] = useState<User>({public_repos: 0, avatar_url: '', login: '', html_url: ''})
+  const [loading, setLoading] = useState(false);
 
   const totalRepos = user.public_repos;
   const pageNumbers: number[] = [];
@@ -27,13 +29,15 @@ function App() {
   for (let i= 1; i <= Math.ceil(totalRepos / 10); i++) {
     pageNumbers.push(i);
   };
-  
+
   const filteredRepos = search.length > 0
     ? repos.filter(repo => repo.name.includes(search))
     : [];
     
   const handleSubmit = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
+    setLoading(true);
+
     fetch(`https://api.github.com/users/${userName}`).then((response) => {
       if (response.ok) {
         return response.json()
@@ -42,39 +46,42 @@ function App() {
       throw new Error('Something went wrong');
     })
     .catch((error) => {
+      setLoading(false);
       setRepos([]);
       setError(error);
     });
 
     fetch(`https://api.github.com/users/${userName}/repos?per_page=10`).then((response) => {
       if (response.ok) {
+        setLoading(false);
         return response.json()
           .then(data => setRepos(data));
       }
       throw new Error('Something went wrong');
     })
     .catch((error) => {
+      setLoading(false);
       setRepos([]);
       setError(error);
-    });
-    
+    });    
   }
 
   const handlePaginate = (number:Number) => {
-      fetch(`https://api.github.com/users/${userName}/repos?per_page=10&page=${number}`).then((response) => {
-        if (response.ok) {
-          return response.json()
-          .then(data => setRepos(data));
-        }
-        throw new Error('Something went wrong');
-      })
-      .catch((error) => {
-        setRepos([]);
-        setError(error);
-      });    
+    setLoading(true);
+    fetch(`https://api.github.com/users/${userName}/repos?per_page=10&page=${number}`).then((response) => {
+      if (response.ok) {
+        setLoading(false);
+        return response.json()
+        .then(data => setRepos(data));
+      }
+      throw new Error('Something went wrong');
+    })
+    .catch((error) => {
+      setLoading(false);
+      setRepos([]);
+      setError(error);
+    });    
   }
-
-  console.log(user)
 
   return (
     <div className="App mt-20">
@@ -101,97 +108,110 @@ function App() {
               </button>
             </form>
           </div>
-          {repos.length > 0 ? (
-            <div className='flex flex-col w-3/5'>
-              <div className='flex items-center justify-center my-7'>
-                <img src={user.avatar_url} alt="" className='h-32 w-32 rounded-full mr-4 border border-blue-400' />
-                <div className='flex flex-col'>
-                  <p className='flex justify-center text-3xl font-semibold'>
-                    {user.login}
-                  </p>
-                  <p className='flex justify-center text-md mb-2'>
-                    Total of repositories: {user.public_repos}
-                  </p>
-                  <a className='flex justify-center' href={user.html_url}>
-                    <GithubLogo 
-                      className='text-slate-400 hover:text-blue-400 text-4xl p-1 border 
-                      rounded-full border-slate-200 hover:bg-slate-100 duration-200' />
-                  </a>                  
-                </div>
-
+          <div>
+            {loading? (
+              <div className='flex flex-col w-[350px] h-[350px] items-center justify-center'>
+                <CircleNotch className='animate-spin text-5xl text-slate-300' />
               </div>
-              <input
-                name='search-repo'
-                className='border-b border-black mb-4' 
-                type="text"
-                placeholder='Filtre o repositório'
-                onChange={e => setSearch(e.target.value)}
-                value={search}
-                />          
-              {search.length > 0 ? ( 
-                <div>                  
-                  {filteredRepos.map(repo => {
-                    return (
-                      <div className='flex' key={repo.name}>
-                        <a className='text-lg font-medium hover:text-blue-800' href={repo.html_url}>
-                          {repo.name}
-                        </a>
-                        <p className='flex ml-2 text-sm text-blue-400 items-center'>
-                          {repo.language}
-                        </p>                                      
+            ) : 
+            (
+              <div>
+                {repos.length > 0 ? (
+                  <div className='flex flex-col w-[350px]'>
+                    <div className='flex items-center justify-center my-7'>
+                      <img src={user.avatar_url} alt="" className='h-32 w-32 rounded-full mr-4 border border-blue-400' />
+                      <div className='flex flex-col'>
+                        <p className='flex justify-center text-3xl font-semibold'>
+                          {user.login}
+                        </p>
+                        <p className='flex justify-center text-md mb-2'>
+                          Total of repositories: {user.public_repos}
+                        </p>
+                        <a className='flex justify-center' href={user.html_url}>
+                          <GithubLogo 
+                            className='text-slate-400 hover:text-blue-400 text-4xl p-1 border 
+                            rounded-full border-slate-200 hover:bg-slate-100 duration-200' />
+                        </a>                  
                       </div>
-                    )
-                  })}
-                </div>
-              ) : (
-                <div className='flex flex-col h-[330px] justify-between'>
-                  <div>
-                    {repos.map(repo => {
-                      return (
-                        <div className='flex' key={repo.name}>
-                          <a className='text-lg font-medium hover:text-blue-600' href={repo.html_url}>
-                            {repo.name}
-                          </a>
-                          <p className='flex ml-1 text-xs text-blue-500 items-center'>
-                            {repo.language}
-                          </p> 
+      
+                    </div>
+                    <input
+                      name='search-repo'
+                      className='border-b border-black mb-4' 
+                      type="text"
+                      placeholder='Filtre o repositório'
+                      onChange={e => setSearch(e.target.value)}
+                      value={search}
+                      />          
+                    {search.length > 0 ? ( 
+                      <div>                  
+                        {filteredRepos.map(repo => {
+                          return (
+                            <div className='flex' key={repo.name}>
+                              <a className='text-lg font-medium hover:text-blue-800' href={repo.html_url}>
+                                {repo.name}
+                              </a>
+                              <p className='flex ml-2 text-sm text-blue-400 items-center'>
+                                {repo.language}
+                              </p>                                      
+                            </div>
+                          )
+                        })}
+                      </div>
+                    ) : 
+                    (
+                      <div className='flex flex-col h-[330px] justify-between'>
+                        <div>
+                          {repos.map(repo => {
+                            return (
+                              <div className='flex' key={repo.name}>
+                                <a className='text-lg font-medium hover:text-blue-600' href={repo.html_url}>
+                                  {repo.name}
+                                </a>
+                                <p className='flex ml-1 text-xs text-blue-500 items-center'>
+                                  {repo.language}
+                                </p> 
+                              </div>
+                            )
+                          })}
                         </div>
-                      )
-                    })}
+                        <nav className='flex justify-center text-md' >
+                          <ul className='flex'>
+                            {pageNumbers.map(number => (
+                              <li key={number} className='m-1'>
+                                <button 
+                                  className='hover:bg-gray-100 hover:text-blue-700 leading-tight py-2 px-3 rounded-md' 
+                                  onClick={() => handlePaginate(number)}>
+                                  {number}
+                                </button>
+                              </li>
+                            ))}
+                          </ul>
+                        </nav>
+                      </div>
+                    )}
+                  </div>              
+                ) : 
+                (
+                  <div className='flex justify-center'>
+                    {error ? (
+                      <div>
+                        <p className='text-red-500 font-bold'>
+                          Usuário não encontrado
+                        </p>
+                      </div>
+                    ) : 
+                    (
+                      <>
+                      </>
+                    )}        
                   </div>
-                <nav className='flex justify-center text-md' >
-                  <ul className='flex'>
-                    {pageNumbers.map(number => (
-                      <li key={number} className='m-1'>
-                        <button 
-                          className='hover:bg-gray-100 hover:text-blue-700 leading-tight py-2 px-3 rounded-md' 
-                          onClick={() => handlePaginate(number)}>
-                          {number}
-                        </button>
-                      </li>
-                    ))}
-                  </ul>
-                </nav>
-                </div>
-              )}
-            </div>
-          
-          ) : (
-            <div className='flex justify-center'>
-              {error ? (
-                <div>
-                  <p className='text-red-500 font-bold'>
-                    Usuário não encontrado
-                  </p>
-                </div>
-              ) : (
-                <>
-                </>
-              )}        
-            </div>
-          )}
+                )}
+              </div>
+            )}         
+          </div>        
         </div>
-      </div>        
+      </div>
     </div>
   );
 }
